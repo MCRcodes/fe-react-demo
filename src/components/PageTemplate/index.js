eb127dfd65ab2f3ccca0a11ab479dc38b4049b38
Martyna Machowiak <martyna.machowiak@gmail.com>
2021-10-19 22:39:05 +0100

    Add index for page elements

---
+++ src/components/PageTemplate/index.js
@@ -0,0 +1,7 @@
+import Footer from "./Footer";
+import MainContent from "./MainContent";
+import Nav from "./Nav";
+import Sidebar from "./Sidebar";
+import SocialMedia from "./SocialMedia";
+
+export { Footer, MainContent, Nav, Sidebar, SocialMedia };
