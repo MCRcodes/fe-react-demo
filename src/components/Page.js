1c5f741eae3d123e73f14c702d95e7c4f846c5b9
Martyna Machowiak <martyna.machowiak@gmail.com>
2021-10-19 22:43:44 +0100

    Add page elements to Page

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,7 +1,16 @@
 import React from "react";
+import { Footer, MainContent, Nav, Sidebar, SocialMedia } from "./PageLayout";
 
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <div>
+      <Sidebar />
+      <Footer />
+      <MainContent />
+      <Nav />
+      <SocialMedia />
+    </div>
+  );
 };
 
 export default Page;
