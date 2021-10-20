eb044da08d5214c32d365f6e45fb7662551934ff
Martyna Machowiak <martyna.machowiak@gmail.com>
2021-10-20 19:47:53 +0100

    Implement login form in App component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import LoginForm from "./components/LoginPage/LoginForm";
 import Page from "./components/Page";
 
 function App() {
   return (
     <div className="App">
+      <LoginForm />
       <Page />
     </div>
   );
