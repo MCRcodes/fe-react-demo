7513041f0cd46dea29c4cee3c2f0bc160ca6b310
Martyna Machowiak <martyna.machowiak@gmail.com>
2021-10-20 21:45:21 +0100

    Demonstrate useEffect usage with catchPokemon

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import LoginForm from "./components/LoginPage/LoginForm";
 import Page from "./components/Page";
+import { catchPokemon } from "./request/catchPokemon";
 
 function App() {
+  const [pokemonData, setPokemonData] = useState([]);
+
+  useEffect(() => {
+    catchPokemon(setPokemonData);
+  }, []);
   return (
     <div className="App">
       <LoginForm />
+      {pokemonData &&
+        pokemonData.map((name) => <p key={name.name}> {name.name} </p>)}
       <Page />
     </div>
   );
